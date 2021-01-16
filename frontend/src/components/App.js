c248bb3d10c9c6951d7422523e9e6fc34d0f9d5f
Patrick Maloney <maloneyp1210@gmail.com>
2021-01-16 17:09:17 -0500

    Refactor index/app and add home page

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,16 +1,23 @@
-import React, { Component } from "react";
-import { render } from "react-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link, Redirect,} from "react-router-dom";
 import LandingPage from '../components/LandingPage';
+import Home from "../components/Home";
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
+const App = () => {
+    const handleSearch = () => {
+
     }
 
-    render() {
-        return <LandingPage />;
-    }
+    return (
+        <Router>
+            <Route exact path = "/">
+                <LandingPage />
+            </Route>
+            <Route path = "/home">
+                <Home onSearch={handleSearch} />
+            </Route>
+        </Router>
+    );
 }
 
-const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+export default App;
